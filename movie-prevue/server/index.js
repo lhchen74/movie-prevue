68ec4ca87a5a48a6622165fb7dd003045ada89d0
lhchen74 <2938449682@qq.com>
2019-05-13 21:17:03 +0800

    use koa-views template-engine middleware

--- movie-prevue/server/index.js
+++ movie-prevue/server/index.js
@@ -1,19 +1,30 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+// const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+
+app.use(views(resolve(__dirname, './views'), {
+    extension:'pug'
+}))
 
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
+    // ctx.type = 'text/html; charset=utf-8'
     // ctx.body = htmlTpl
 
     // ctx.body = ejs.render(ejsTpl, {
     //     name: 'Ejs'
     // })
 
-    ctx.body = pug.render(pugTpl, {
-        name: 'Pug'
+    // ctx.body = pug.render(pugTpl, {
+    //     name: 'Pug'
+    // })
+
+    await ctx.render('index', {
+        name: 'koa-views and pug'
     })
 })
 
