f8c4ff3c4c625030a797146ef59db87a177b6144
lhchen74 <2938449682@qq.com>
2019-05-13 20:26:27 +0800

    test static html tpl

---
+++ movie-prevue/server/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koa Server Html</title>
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.js"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Hello Koa</h1>
+            </div>
+            <div class="col-md-4">
+                <p>static page</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+`
